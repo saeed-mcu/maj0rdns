c61c4f101b801f2f7dac93453f0c0af0c973aee0
Milad Soltanian <maj0rmil4d@gmail.com>
2022-01-22 10:23:31 +0330

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -4,4 +4,6 @@ docker rm $(docker ps -a | grep maj0rdnscontainer | cut -d " " -f 1)
 
 docker build . -t maj0rdns:latest
 
-docker run -d -it --cap-add=NET_ADMIN -p 53:53/udp -p 443:443 -p 80:80 -e IP=PublicIpOfContainer --name maj0rdnscontainer maj0rdns:latest 
+docker run -d -it --cap-add=NET_ADMIN -p 53:53/udp -p 443:443 -p 80:80 -e IP=162.55.10.138 --name maj0rdnscontainer maj0rdns:latest 
+
+docker update --restart=always $(docker ps -a | grep maj0rdnscontainer | cut -d " " -f 1)
